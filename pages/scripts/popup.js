7e6a73a7098cf4cf1d6cea08d626da40dc763eaa
medeiros-augusto <augustomedeiros047@gmail.com>
2023-06-01 22:38:43 -0300

    add popup para login

---
+++ pages/scripts/popup.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('#login')
+const popup = document.querySelector('.popup-wrapper')
+const closebutton = document.querySelector('.popup-close')
+const geral = document.querySelector('.pg-footer')
+
+button.addEventListener('click', () => {
+    popup.style.display = 'block'
+    geral.style.display = 'none'
+})
+
+popup.addEventListener('click', event => {
+    const classnameofclickedelement = event.target.classList[0];
+    console.log(classnameofclickedelement)
+    const classnames = ['popup-link', 'popup-close', 'popup-wrapper']
+    const closepopup = classnames.some(classname => classname === classnameofclickedelement)
+    if (closepopup) {
+        popup.style.display = 'none'
+    }
+})
