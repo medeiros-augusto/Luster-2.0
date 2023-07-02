99e3352d4b477e6e99fcb4833701828a79f19de8
golerme <golerme.boettger@gmail.com>
2023-07-02 20:31:58 -0300

    comentacao

--- pages/scripts/conteudo.js
+++ pages/scripts/conteudo.js
@@ -1,16 +1,28 @@
-var mais = document.querySelector('#mais')
-var menos = document.querySelector('#menos')
-const conteudo = document.querySelector('.conteudo p')
+// Seleciona o elemento com o id "mais"
+var mais = document.querySelector("#mais");
 
-mais.addEventListener('click', function () {
-    conteudo.style.display="block"
-    mais.style.display="none"
-    menos.style.display="inline"
+// Seleciona o elemento com o id "menos"
+var menos = document.querySelector("#menos");
 
-})
-menos.addEventListener('click', function () {
-    conteudo.style.display="none"
-    mais.style.display="inline"
-    menos.style.display="none"
+// Seleciona o elemento com a classe "conteudo p"
+const conteudo = document.querySelector(".conteudo p");
 
-})
+// Adiciona um ouvinte de evento de clique ao elemento "mais"
+mais.addEventListener("click", function () {
+  // Altera o estilo de exibição do elemento de conteúdo para "block"
+  conteudo.style.display = "block";
+  // Altera o estilo de exibição do elemento "mais" para "none" (oculto)
+  mais.style.display = "none";
+  // Altera o estilo de exibição do elemento "menos" para "inline" (exibido)
+  menos.style.display = "inline";
+});
+
+// Adiciona um ouvinte de evento de clique ao elemento "menos"
+menos.addEventListener("click", function () {
+  // Altera o estilo de exibição do elemento de conteúdo para "none" (oculto)
+  conteudo.style.display = "none";
+  // Altera o estilo de exibição do elemento "mais" para "inline" (exibido)
+  mais.style.display = "inline";
+  // Altera o estilo de exibição do elemento "menos" para "none" (oculto)
+  menos.style.display = "none";
+});
