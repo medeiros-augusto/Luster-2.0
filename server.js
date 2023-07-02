df75d89a28998bb94961ca3158bb50ef7525e45a
medeiros-augusto <augustomedeiros047@gmail.com>
2023-07-02 10:56:09 -0300

    puxa dado do bd

--- server.js
+++ server.js
@@ -25,36 +25,6 @@ connection.connect(function(err) {
     }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-//login
-
-app.post('/login', (req, res) => {
-    let username = req.body.mail;
-    let password = req.body.senha;
-
-    connection.query("SELECT * FROM usuario where email_usuario = '" + username + "'", function(err, rows, fields) {
-        console.log("Results:", rows);
-        if (!err) {
-            if (rows.length > 0) {
-
-                if (rows[0].senha_usuario === password) {
-                    res.send('Login com Sucesso!!!');
-                } else {
-                    res.send('Senha incorreta');
-                }
-
-            } else {
-                res.send('Login Falhou - Usuário não cadastrado');
-            }
-        } else {
-            console.log("Erro: Consulta não realizada", err);
-            res.send('Login failed');
-        }
-    });
-});
-
 //Cadastro
 
 app.post('/cadastro', (req, res) => {
@@ -74,6 +44,67 @@ app.post('/cadastro', (req, res) => {
         }
     );
 })
-app.listen(3001, () => {
-    console.log('Servidor rodando na porta 3000!')
+
+//login
+
+app.post('/login', (req, res) => {
+    let username = req.body.mail;
+    let password = req.body.senha;
+
+    connection.query("SELECT * FROM usuario where email_usuario = '" + username + "'", function(err, rows, fields) {
+        console.log("Results:", rows);
+        if (!err) {
+            if (rows.length > 0) {
+
+                if (rows[0].senha_usuario === password) {
+                    res.send('Login com Sucesso!!!'); //ALTERAR 
+                } else {
+                    res.send('Senha incorreta');
+                }
+
+            } else {
+                res.send('Login Falhou - Usuário não cadastrado');
+            }
+        } else {
+            console.log("Erro: Consulta não realizada", err);
+            res.send('Login failed');
+        }
+    });
+});
+
+//Consulta do Saldo do usuário
+const fs = require('fs');
+const cheerio = require('cheerio');
+
+  app.get('/', function(req, res) {
+    connection.query('SELECT saldo_usuario FROM usuario', function(error, results, fields) {
+        if (error) throw error;
+        const saldo = parseFloat(results[0].saldo_usuario);
+        console.log(saldo)
+        const filePath = 'index.html';
+        
+        fs.readFile(filePath, 'utf8', function(error, data) {
+            if (error) throw error;
+      
+            // Carrega o HTML usando o cheerio
+            const $ = cheerio.load(data);
+      
+            // Encontra a tag <p> com o id "header-saldo" e altera seu conteúdo
+            $('#header-saldo').text(`R$ ${saldo}`);
+      
+            // Obtém o HTML modificado
+            const htmlModificado = $.html();
+      
+            fs.writeFile(filePath, htmlModificado, 'utf8', function(error) {
+                if (error) throw error;
+        
+                // Envie o HTML modificado como resposta
+                res.send(htmlModificado);
+              });
+          });
+      });
+  });
+
+app.listen(3010, () => {
+    console.log('Servidor rodando na porta 3010!')
 })
