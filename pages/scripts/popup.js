58572d8f734f575e5bddc8a022ee371b8951faaa
golerme <golerme.boettger@gmail.com>
2023-07-02 20:32:07 -0300

    comentacao poha

--- pages/scripts/popup.js
+++ pages/scripts/popup.js
@@ -1,52 +1,69 @@
-const button = document.querySelector('#login')
-const signup = document.querySelector('.popup-link')
-const popup = document.querySelector('.popup-wrapper')
-const popupcad = document.querySelector('.popup-total')
-const closebutton = document.querySelector('.popup-close')
-const volta = document.querySelector('#pop-volta')
-const recarga = document.querySelector('#recarga')
-var poprecarga = document.querySelector('.popup-recarga')
+const button = document.querySelector("#login"); // Seleciona o elemento com id "login" e armazena na variável button
+const signup = document.querySelector(".popup-link"); // Seleciona o elemento com classe "popup-link" e armazena na variável signup
+const popup = document.querySelector(".popup-wrapper"); // Seleciona o elemento com classe "popup-wrapper" e armazena na variável popup
+const popupcad = document.querySelector(".popup-total"); // Seleciona o elemento com classe "popup-total" e armazena na variável popupcad
+const closebutton = document.querySelector(".popup-close"); // Seleciona o elemento com classe "popup-close" e armazena na variável closebutton
+const volta = document.querySelector("#pop-volta"); // Seleciona o elemento com id "pop-volta" e armazena na variável volta
+const recarga = document.querySelector("#recarga"); // Seleciona o elemento com id "recarga" e armazena na variável recarga
+var poprecarga = document.querySelector(".popup-recarga"); // Seleciona o elemento com classe "popup-recarga" e armazena na variável poprecarga
 
-button.addEventListener('click', () => {
-    popup.style.display = 'block'
-})
-signup.addEventListener('click', () => {
-    popup.style.display = 'none'
-    popupcad.style.display = 'block'
-})
-volta.addEventListener('click', () => {
-    popup.style.display = 'block'
-    popupcad.style.display = 'none'
-})
-recarga.addEventListener('click', () => {
-    poprecarga.style.display = 'block'
-})
+button.addEventListener("click", () => {
+  // Adiciona um ouvinte de eventos de clique ao botão "login"
+  popup.style.display = "block"; // Altera o estilo de exibição do popup para "block" (exibido)
+});
 
-popup.addEventListener('click', event => {
-    const classnameofclickedelement = event.target.classList[0];
-    console.log(classnameofclickedelement)
-    const classnames = ['popup-close', 'popup-wrapper']
-    const closepopup = classnames.some(classname => classname === classnameofclickedelement)
-    if (closepopup) {
-        popup.style.display = 'none'
-    }
-})
+signup.addEventListener("click", () => {
+  // Adiciona um ouvinte de eventos de clique ao elemento "popup-link"
+  popup.style.display = "none"; // Altera o estilo de exibição do popup para "none" (oculto)
+  popupcad.style.display = "block"; // Altera o estilo de exibição do popupcad para "block" (exibido)
+});
 
-popupcad.addEventListener('click', event => {
-    const classnameofclickedelement = event.target.classList[0];
-    console.log(classnameofclickedelement)
-    const classnames = ['popup-close', 'popup-total']
-    const closepopup = classnames.some(classname => classname === classnameofclickedelement)
-    if (closepopup) {
-        popupcad.style.display = 'none'
-    }
-})
-poprecarga.addEventListener('click', event => {
-    const classnameofclickedelement = event.target.classList[0];
-    console.log(classnameofclickedelement)
-    const classnames = ['popup-close', 'popup-recarga']
-    const closepopup = classnames.some(classname => classname === classnameofclickedelement)
-    if (closepopup) {
-        poprecarga.style.display = 'none'
-    }
-})
+volta.addEventListener("click", () => {
+  // Adiciona um ouvinte de eventos de clique ao elemento com id "pop-volta"
+  popup.style.display = "block"; // Altera o estilo de exibição do popup para "block" (exibido)
+  popupcad.style.display = "none"; // Altera o estilo de exibição do popupcad para "none" (oculto)
+});
+
+recarga.addEventListener("click", () => {
+  // Adiciona um ouvinte de eventos de clique ao elemento com id "recarga"
+  poprecarga.style.display = "block"; // Altera o estilo de exibição do poprecarga para "block" (exibido)
+});
+
+popup.addEventListener("click", (event) => {
+  // Adiciona um ouvinte de eventos de clique ao elemento "popup-wrapper"
+  const classnameofclickedelement = event.target.classList[0]; // Obtém a classe do elemento clicado dentro do popup
+  console.log(classnameofclickedelement); // Imprime a classe do elemento clicado no console
+  const classnames = ["popup-close", "popup-wrapper"]; // Array de classes que indicam que o popup deve ser fechado
+  const closepopup = classnames.some(
+    (classname) => classname === classnameofclickedelement
+  ); // Verifica se a classe do elemento clicado está presente no array de classes
+  if (closepopup) {
+    popup.style.display = "none"; // Fecha o popup alterando o estilo de exibição para "none" (oculto)
+  }
+});
+
+popupcad.addEventListener("click", (event) => {
+  // Adiciona um ouvinte de eventos de clique ao elemento "popup-total"
+  const classnameofclickedelement = event.target.classList[0]; // Obtém a classe do elemento clicado dentro do popupcad
+  console.log(classnameofclickedelement); // Imprime a classe do elemento clicado no console
+  const classnames = ["popup-close", "popup-total"]; // Array de classes que indicam que o popupcad deve ser fechado
+  const closepopup = classnames.some(
+    (classname) => classname === classnameofclickedelement
+  ); // Verifica se a classe do elemento clicado está presente no array de classes
+  if (closepopup) {
+    popupcad.style.display = "none"; // Fecha o popupcad alterando o estilo de exibição para "none" (oculto)
+  }
+});
+
+poprecarga.addEventListener("click", (event) => {
+  // Adiciona um ouvinte de eventos de clique ao elemento "popup-recarga"
+  const classnameofclickedelement = event.target.classList[0]; // Obtém a classe do elemento clicado dentro do poprecarga
+  console.log(classnameofclickedelement); // Imprime a classe do elemento clicado no console
+  const classnames = ["popup-close", "popup-recarga"]; // Array de classes que indicam que o poprecarga deve ser fechado
+  const closepopup = classnames.some(
+    (classname) => classname === classnameofclickedelement
+  ); // Verifica se a classe do elemento clicado está presente no array de classes
+  if (closepopup) {
+    poprecarga.style.display = "none"; // Fecha o poprecarga alterando o estilo de exibição para "none" (oculto)
+  }
+});
